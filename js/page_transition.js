1e97a82ddb469a76d35ce368341e3511800d4b86
Dan5762 <Daniel@alberg-long.co.uk>
2022-10-13 13:39:54 +0100

    created structure of site

---
+++ js/page_transition.js
@@ -0,0 +1,19 @@
+function up(newLocation) {
+  location.href = newLocation
+  return false;
+}
+
+function down(newLocation) {
+  location.href = newLocation
+  return false;
+}
+
+function left(newLocation) {
+  location.href = newLocation
+  return false;
+}
+
+function right(newLocation) {
+  location.href = newLocation
+  return false;
+}
